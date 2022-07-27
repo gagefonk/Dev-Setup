82e45f2bd29c53e8560eac126d83d6921f687e02
Gage <gagefonk@gmail.com>
2022-07-26 18:47:34 -0700

    Update terminalSetup.sh

--- terminalSetup.sh
+++ terminalSetup.sh
@@ -1,48 +1,175 @@
 #!/bin/bash
 
-# Create config directory(s)
-mkdir ~/.config
-mkdir ~/.config/tmux
+### VARIABLES
+needsBrewUpdate=false
 
-# HomeBrew
+### Functions
+
+# Create Directory
+# Create them only if they dont exist already.
+createDirectories()
+{
+mkdir -p ~/.config
+}
+
+# Homebrew
+# Install Homebrew
+installHomebrew()
+{
 git clone https://github.com/Homebrew/brew /opt/homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
+}
 
-# Install the Software
+# Homebrew Updates
+homebrewUpdate()
+{
+if [ "$needsBrewUpdate" = true ]; then
+brew update
+fi
+}
 
-#iterm2
-brew install --cask iterm2
-#ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#powerlevel10k theme for ohmyzsh
-brew install romkatv/powerlevel10k/powerlevel10k
-echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+# Install/Update Iterm2
+installIterm()
+{
+if brew ls --versions iterm2 > /dev/null; then
+  brew install --cask iterm2
+else
+  # exists - queue update
+  $needsBrewUpdate=true
+fi
+}
 
-####plugins
+# Install OhMyZSH
+installOhMyZSH()
+{
+if [ d- ~/.oh-my-zsh ]; then
+# update it
+omz update
+else
+# install it
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+}
 
-#zsh-autocompletions
-brew install zsh-autosuggestions
-#zsh syntax highlighting
-brew install zsh-syntax-highlighting
-#pull ohmyzsh config file
-echo "pulling ohmyzsh config file"
-curl -o ~/.zshrc https://raw.githubusercontent.com/gagefonk/Dev-Setup/master/.zshrc
-#pull p10k config file
-echo "pulling p10k config file"
-curl -o ~/.p10k.zsh https://raw.githubusercontent.com/gagefonk/Dev-Setup/master/.p10k.zsh
+# P10k Theme
+installP10kTheme()
+{
+if brew ls --versions romkatv/powerlevel10k/powerlevel10k > /dev/null; then
+  brew install romkatv/powerlevel10k/powerlevel10k
+  echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+else
+  $needsBrewUpdate=true
+fi
+}
 
-#neovim
-brew install neovim
-#nerd font hack
-brew tap homebrew/cask-fonts
-brew install --cask font-hack-nerd-font
+# ZSH Plugins
+# ZSH AutoCompletion
+installAutoCompletion()
+{
+if brew ls --version zsh-autosuggestions > /dev/null; then
+  brew install zsh-autosuggestions
+else
+  $needsBrewUpdate=true
+fi
+}
 
-# Download the config .dotfiles from git
+# ZSH Syntax Highlighting
+installSyntaxHighlighting()
+{
+if brew ls --version zsh-syntax-highlighting > /dev/null; then
+  brew install zsh-syntax-highlighting
+else
+  $needsBrewUpdate=true
+fi
+}
+
+# Hack Nerd Font
+installHackNerdFont()
+{
+if brew ls --version font-hack-nerd-font > /dev/null; then
+  brew tap homebrew/cask-fonts
+  brew install --cask font-hack-nerd-font
+else
+  $needsBrewUpdate=true
+fi
+}
+
+# NVIM
+installNVIM()
+{
+if brew ls --version neovim > /dev/null; then
+  brew install neovim
+else
+  $needsBrewUpdate=true
+fi
+}
+
+# TMUX
+installTmux()
+{
+if brew ls --version tmux > /dev/null; then
+  brew install tmux
+else
+  $needsBrewUpdate=true
+fi
+}
+
+# Install Config Files
+installConfigs()
+{
+# Tmux
 echo "pulling tmux config file"
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/gagefonk/Dev-Setup/master/tmux.conf
+# ZSHRC
+echo "pulling ohmyzsh config file"
+curl -o ~/.zshrc https://raw.githubusercontent.com/gagefonk/Dev-Setup/master/.zshrc
+# P10K
+echo "pulling p10k config file"
+curl -o ~/.p10k.zsh https://raw.githubusercontent.com/gagefonk/Dev-Setup/master/.p10k.zsh
+}
 
-# set zsh source file?
-zsh
+
+
+
+### SCRIPT
+
+# Create Directories
+createDirectories
+
+# HomeBrew
+installHomebrew
+
+# Iterm
+installIterm
+
+# OhMyZSH
+installOhMyZSH
+
+# P10k Theme
+installP10kTheme
+
+# ZSH Plugins
+# AutoCompletion
+installAutoCompletion
+# Syntax Highlighting
+installSyntaxHighlighting
+
+# Hack Nerd font
+installHackNerdFont
+
+# NVIM
+installNVIM
+
+# Tmux
+installTmux
+
+# Install Config Files
+installConfigs
+
+# Brew Updates
+homebrewUpdate
+
+# Source the ZSHRC file just incase
 source ~/.zshrc
