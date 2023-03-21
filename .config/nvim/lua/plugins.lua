b715e50d9c5b799012e030c2def2528c8eeaa2ff
Gage <gagefonk@gmail.com>
2023-03-21 16:35:35 -0700

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,13 +1,32 @@
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use Protected Call so we dont error our
 local status, packer = pcall(require, "packer")
 if (not status) then
   print("Packer is not installed")
   return
 end
 
+-- Have packer use floating window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float {border = "rounded" }
+    end,
+  },
+}
+
 vim.cmd [[packadd packer.nvim]]
 
-packer.startup(function(use)
+return packer.startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'nvim-lua/popup.nvim'
   use {
     'svrana/neosolarized.nvim',
     requires = { 'tjdevries/colorbuddy.nvim' }
@@ -50,4 +69,9 @@ packer.startup(function(use)
 
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim' -- For git blame & browse
+    
+  -- Automatically set up config after cloning packer.nvim
+    if PACKER_BOOSTRAP then
+      require("packer").sync()
+    end
 end)
