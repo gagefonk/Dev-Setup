7c366ad89d3db4eb64c271df54a2ff8e4962278a
Gage <gagefonk@gmail.com>
2023-03-22 15:33:28 -0700

    update

--- .config/nvim/lua/after/plugin/treesitter.rc.lua
+++ .config/nvim/lua/after/plugin/treesitter.rc.lua
@@ -1,5 +1,5 @@
-require 'nvim-treesitter.configs' .setup {
-  ensure_installed = { "lua", "ruby", "vim", "typescript", "javascript", "python", "html", "css" },
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = { "lua",  "vim", "typescript", "javascript", "python", "html", "css" },
 
   sync_install = false,
   auto_install = true,
