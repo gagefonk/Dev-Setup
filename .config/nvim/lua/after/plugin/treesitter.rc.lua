63b8cca8fc49ba533f60bfa0213ffd96ec6ec604
Gage <gagefonk@gmail.com>
2023-03-22 15:47:16 -0700

    update

--- .config/nvim/lua/after/plugin/treesitter.rc.lua
+++ .config/nvim/lua/after/plugin/treesitter.rc.lua
@@ -1,9 +1,30 @@
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua",  "vim", "typescript", "javascript", "python", "html", "css" },
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if (not status) then return end
 
-  sync_install = false,
-  auto_install = true,
+ts.setup {
   highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "markdown",
+    "markdown_inline",
+    "tsx",
+    "json",
+    "yaml",
+    "swift",
+    "css",
+    "html",
+    "lua"
+  },
+  autotag = {
     enable = true,
   },
 }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
