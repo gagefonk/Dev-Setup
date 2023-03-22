643ec7f66dfa0faf8827ba4a9fbe8a0b2aa5030a
Gage <gagefonk@gmail.com>
2023-03-21 17:06:39 -0700

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -44,10 +44,10 @@ return packer.startup(function(use)
 
 --   use 'glepnir/lspsaga.nvim' -- LSP UIs
 --   use 'L3MON4D3/LuaSnip'
---   use {
---     'nvim-treesitter/nvim-treesitter',
---     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
---   }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  }
 --   use 'kyazdani42/nvim-web-devicons' -- File icons
 --   use 'nvim-telescope/telescope.nvim'
 --   use 'nvim-telescope/telescope-file-browser.nvim'
