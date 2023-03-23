1bb715107ee1f2466b4be39ec97d9fa9fe5a30fa
Gage <gagefonk@gmail.com>
2023-03-23 15:25:03 -0700

    update

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,10 +10,12 @@ end
 
 telescope.setup({
     defaults = {
-        i = {
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+        mappings = {
+            i = {
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            }
         }
     }
 })
