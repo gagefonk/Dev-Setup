afbce518c0519672002fe8ff0a841fd0dba6349c
Gage <gagefonk@gmail.com>
2023-03-23 15:15:02 -0700

    update

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -3,24 +3,37 @@ vim.g.maplocalleader = " "
 
 local keymap = vim.keymap
 
-keymap.set('i', 'jk', '<ESC>')                  -- escape insert
+-- escape insert
+keymap.set('i', 'jk', '<ESC>')
 
-keymap.set('n', '<leader>nh', ':nohl<CR>')      -- clear search
+-- clear search
+keymap.set('n', '<leader>nh', ':nohl<CR>')
 
-keymap.set('n', 'x', '_x')                     -- delete without copying
+-- delete without copying
+keymap.set('n', 'x', '_x')
 
-keymap.set('n', '<leader>+', '<C-a>')           -- increment number 
-keymap.set('n', '<leader>-', '<C-x>')           -- decrement number
+-- increment number 
+keymap.set('n', '<leader>+', '<C-a>')
+-- decrement number
+keymap.set('n', '<leader>-', '<C-x>')
 
-keymap.set('n', '<leader>sv', '<C-w>V')         -- split window vertically
-keymap.set('n', '<leader>sh', '<C-w>s')         -- split window horizontally
-keymap.set('n', '<leader>se', '<C-w>=')         -- split window equal width
-keymap.set('n', '<leader>sx', ':close<CR>')     -- close split window
+-- split window vertically
+keymap.set('n', '<leader>sv', '<C-w>V')
+-- split window horizontally
+keymap.set('n', '<leader>sh', '<C-w>s')
+-- split window equal width
+keymap.set('n', '<leader>se', '<C-w>=')
+-- close split window
+keymap.set('n', '<leader>sx', ':close<CR>')
 
-keymap.set('n', '<leader>to', ':tabnew<CR>')    -- open new tab
-keymap.set('n', '<leader>tx', ':tabclose<CR>')  -- close tab
-keymap.set('n', '<leader>tn', ':tabn<CR>')     -- go to next tab
-keymap.set('n', '<leader>tp', ':tabp<CR>')     -- go to previous tab
+-- open new tab
+keymap.set('n', '<leader>to', ':tabnew<CR>')
+-- close tab
+keymap.set('n', '<leader>tx', ':tabclose<CR>')
+-- go to next tab
+keymap.set('n', '<leader>tn', ':tabn<CR>')
+-- go to previous tab
+keymap.set('n', '<leader>tp', ':tabp<CR>')
 
 -- PLUGIN KEYMAPS
 
