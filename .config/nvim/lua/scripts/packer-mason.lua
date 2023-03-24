c6dda6af711f69899cfe1db9426957feba05ad29
Gage <gagefonk@gmail.com>
2023-03-23 17:48:07 -0700

    update

--- .config/nvim/lua/scripts/packer-mason.lua
+++ .config/nvim/lua/scripts/packer-mason.lua
@@ -3,11 +3,28 @@ local api = vim.api
 
 -- Define a function to run :Mason
 local function run_mason()
-  api.nvim_command('MasonInstallAll')
+  api.nvim_command('Mason')
 end
 
 -- Register an autocmd to run :Mason after Packer completes
-vim.cmd([[autocmd User PackerComplete lua run_mason()]])
+vim.cmd([[autocmd User PackerComplete lua require("my_plugin_setup").run_mason()]])
 
--- Run :PackerSync
-api.nvim_command('PackerSync')
+-- Define the Packer setup function
+function setup_packer()
+  api.nvim_command('packadd packer.nvim')
+  require('packer').sync()
+end
+
+-- Define the Mason setup function
+function setup_mason()
+  api.nvim_command('packadd nvim-mason')
+end
+
+-- Define the function to run Packer and Mason setup
+function setup()
+  setup_packer()
+  setup_mason()
+end
+
+-- Run Packer and Mason setup
+setup()
