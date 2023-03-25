ea929cb6d0ee21743575fef8612b9bea144b8894
Gage <gagefonk@gmail.com>
2023-03-25 12:04:02 -0700

    update

--- .config/nvim_old/lua/plugins/comment.lua
+++
@@ -1,7 +0,0 @@
-local setup, comment = pcall(require, "Comment")
-if not setup then
-    print("Failed to load Comment.")
-    return
-end
-
-comment.setup()
--- .config/nvim_old/lua/plugins/gitsigns.lua
+++
@@ -1,7 +0,0 @@
-local setup, gitsigns = pcall(require, "gitsigns")
-if not setup then
-    print("Failed to load gitsigns.")
-    return
-end
-
-gitsigns.setup()
