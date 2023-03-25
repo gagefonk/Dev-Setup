d1551982dcf9b0669f956c0a05f7c7ca4c381f11
Gage <gagefonk@gmail.com>
2023-03-25 15:02:04 -0700

    update

--- terminalSetup.sh
+++ terminalSetup.sh
@@ -167,4 +167,4 @@ defaults import com.googlecode.iterm2 ~/.config/iterm2/com.googlecode.iterm2.pli
 #nvim -c "luafile ~/.config/nvim/lua/scripts/packer-mason.lua"
 #echo "Installation complete, please close terminal/iterm2 and relaunch iterm"
 echo "Setting up LunarVim..."
-echo LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
