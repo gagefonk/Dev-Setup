9fb9b6ec32cdb3a478c6d688e5e1ac37fa38242c
Gage <gagefonk@gmail.com>
2023-03-30 14:04:53 -0700

    update

---
+++ .config/nvim/lua/core/lazy.lua
@@ -0,0 +1,24 @@
+-- BOOTSTRAP
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- PCALL
+local ok, lazy = pcall(require, 'lazy')
+if not ok then return end
+
+-- GET PLUGIN LIST
+local plugins = require("plugins.plugins")
+local opts = {}
+
+-- SETUP
+lazy.setup(plugins, opts)
---
+++ .config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,47 @@
+local plugins = {
+    -- COLOR SCHEME
+    'folke/tokyonight.nvim',
+    -- FILE EXPLORER
+    'nvim-tree/nvim-tree.lua',
+    -- LUALINE
+    'nvim-lualine/lualine.nvim',
+
+
+
+    -- -- TREE SITTER
+    -- 'nvim-treesitter/nvim-treesitter',
+    -- 'nvim-tree/nvim-web-devicons',
+    -- -- FILE EXPLORER
+    -- 'nvim-tree/nvim-tree.lua',
+    -- -- LUALINE
+    -- 'nvim-lualine/lualine.nvim',
+    -- -- TELESCOPE
+    -- {
+    --     'nvim-telescope/telescope.nvim',
+    --     branch = '0.1.x',
+    --     dependencies = { {'nvim-lua/plenary.nvim'} }
+    -- },
+    -- -- AUTO COMPLETION
+    -- 'hrsh7th/nvim-cmp',
+    -- 'hrsh7th/cmp-nvim-lsp',
+    -- -- SNIPPETS
+    -- 'L3MON4D3/LuaSnip',
+    -- 'saadparwaiz1/cmp_luasnip',
+    -- 'rafamadriz/friendly-snippets',
+    -- -- LSP SERVERS
+    -- 'williamboman/mason.nvim',
+    -- 'williamboman/mason-lspconfig.nvim',
+    -- -- LSP CONFIG
+    -- 'neovim/nvim-lspconfig',
+    -- 'RRethy/vim-illuminate',
+    -- -- LINT & FORMAT
+    -- 'jose-elias-alvarez/null-ls.nvim',
+    -- 'jayp0521/mason-null-ls.nvim',
+    -- 'lukas-reineke/indent-blankline.nvim',
+    -- -- BUFFERS
+    -- 'akinsho/bufferline.nvim',
+    -- 'moll/vim-bbye',
+
+}
+
+return plugins
