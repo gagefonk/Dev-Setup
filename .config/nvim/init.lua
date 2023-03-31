f734c12f4be64a141f37144dbf69008d5c553b91
Gage <gagefonk@gmail.com>
2023-03-30 17:28:56 -0700

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,14 @@
-require('core.keymaps')
-require('core.options')
-require('core.lazy')
+vim.api.nvim_echo({
+  {
+    "Do not use this repository directly\n",
+    "ErrorMsg",
+  },
+  {
+    "Please check the docs on how to get started with LazyVim\n",
+    "WarningMsg",
+  },
+  { "Press any key to exit", "MoreMsg" },
+}, true, {})
+
+vim.fn.getchar()
+vim.cmd([[quit]])
