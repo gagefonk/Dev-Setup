1b93474cdc05302a10b723250f2c5dea4c7e07e5
Gage <gagefonk@gmail.com>
2023-03-30 22:08:42 -0700

    update

---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,21 @@
+return {
+    'nvim-neo-tree/neo-tree.nvim',
+    cmd = 'Neotree',
+    branch = 'v2.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons',
+        'MunifTanjim/nui.nvim',
+    },
+    config = {
+        filesystem = {
+            follow_current_file = true,
+            hijack_netrw_behavior = 'open_current',
+            filtered_items = {
+                visible = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+            },
+        },
+    },
+}
